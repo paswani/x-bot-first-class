e0d9123f63bd8d4b822e78672b741c4c602beee2
zubairv85 <zubairv85@gmail.com>
2017-04-08 03:35:41 -0500

    Revert "Added a thank you and goodbye intent to the rejection notice dialog."
    This reverts commit d053959e191f2ee6b879d0bac8b683a47b88231c.

--- X-Bot First Class/Dialogs/RejectionNoticeDialog.cs
+++ X-Bot First Class/Dialogs/RejectionNoticeDialog.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
-using Microsoft.Bot.Connector;
 using X_Bot_First_Class.Common;
 
 namespace X_Bot_First_Class.Dialogs
@@ -31,77 +29,17 @@ namespace X_Bot_First_Class.Dialogs
             var entities = result.Entities;
             var jobIds = entities.Select(entity => entity.Entity).ToList<string>();
             var response = "I'm sorry, I did not understand your response.";
-            var name = string.Empty;
-            context.UserData.TryGetValue<string>("name", out name);
 
             if (jobIds.Count == 1)
             {
-                response = "OK. I have applied for the 1 position you selected. Good luck!";
+                response = "Ok. I have applied for the 1 position you selected. Good luck!";
             }
             else if (jobIds.Count > 1)
             {
-                response = "OK. I have applied for the " + jobIds.Count + " positions you selected. Good luck!";
+                response = "Ok. I have applied for the " + jobIds.Count + " positions you selected. Good luck!";
             }
 
-            dynamic channelData = new ExpandoObject();
-            channelData.HtmlBody = string.Format(Resources.suggestionApplyEmailTemplate, response);
-
-            IMessageActivity message = Activity.CreateMessageActivity();
-            message.From = context.Activity.From;
-            message.Recipient = context.Activity.Recipient;
-            message.Conversation = context.Activity.Conversation;
-            message.Locale = "en-us";
-            message.ChannelData = channelData;
-
-            await context.PostAsync(message);
-
-            context.Done<string>(null);
-        }
-
-        /// <summary>
-        /// Goodbye intent.
-        /// </summary>
-        /// <param name="context">The context.</param>
-        /// <param name="result">The result.</param>
-        /// <returns></returns>
-        [LuisIntent("ThankYou")]
-        public async Task ThankYou(IDialogContext context, LuisResult result)
-        {
-            dynamic channelData = new ExpandoObject();
-            channelData.HtmlBody = Resources.thankYouEmailTemplate;
-
-            IMessageActivity message = Activity.CreateMessageActivity();
-            message.From = context.Activity.From;
-            message.Recipient = context.Activity.Recipient;
-            message.Conversation = context.Activity.Conversation;
-            message.Locale = "en-us";
-            message.ChannelData = channelData;
-
-            await context.PostAsync(message);
-
-            context.Done<string>(null);
-        }
-
-        /// <summary>
-        /// Goodbye intent.
-        /// </summary>
-        /// <param name="context">The context.</param>
-        /// <param name="result">The result.</param>
-        /// <returns></returns>
-        [LuisIntent("Goodbye")]
-        public async Task Goodbye(IDialogContext context, LuisResult result)
-        {
-            dynamic channelData = new ExpandoObject();
-            channelData.HtmlBody = Resources.goodbyeEmailTemplate;
-
-            IMessageActivity message = Activity.CreateMessageActivity();
-            message.From = context.Activity.From;
-            message.Recipient = context.Activity.Recipient;
-            message.Conversation = context.Activity.Conversation;
-            message.Locale = "en-us";
-            message.ChannelData = channelData;
-
-            await context.PostAsync(message);
+            await context.PostAsync(response);
 
             context.Done<string>(null);
         }
