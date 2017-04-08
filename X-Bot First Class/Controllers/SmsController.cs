97e282905674750e9e423f327fb5b8fa61867bd0
thor-schueler <thor.t.schueler@gmail.com>
2017-04-08 07:19:38 -0500

    Tested SMS Applicant Acceptance. adjust message.

--- X-Bot First Class/Controllers/SmsController.cs
+++ X-Bot First Class/Controllers/SmsController.cs
@@ -63,7 +63,6 @@ namespace X_Bot_First_Class
         /// Sends the an SMS informing the applicant of job selection and invites the .
         /// </summary>
         /// <param name="phoneNumber">The phone number.</param>
-        /// <param name="name">The name.</param>
         /// <param name="jobId">The id of the job the applicant is accepted for.</param>
         /// <returns>Async HttpResponseMessage</returns>
         [HttpGet]
@@ -71,19 +70,20 @@ namespace X_Bot_First_Class
         public async Task<HttpResponseMessage> ApplicantAccepted(string phoneNumber, string jobId)
         {
             if (string.IsNullOrEmpty(phoneNumber) || string.IsNullOrEmpty(jobId)) return Request.CreateResponse(HttpStatusCode.BadRequest);
-            if (!phoneNumber.StartsWith("+")) phoneNumber = string.Concat("+", phoneNumber);
-
+            
             // find application
             Application app = null;
             Applicant a = await ApplicantFactory.GetApplicantByPhone(phoneNumber);
             if (a == null) return Request.CreateResponse(HttpStatusCode.NotFound);
             if (!a.Applications.Keys.Contains(jobId)) return Request.CreateResponse(HttpStatusCode.NotFound);
             app = a.Applications[jobId];
+
+            if (!phoneNumber.StartsWith("+")) phoneNumber = string.Concat("+", phoneNumber);
             var payload = new MessagePayload()
             {
                 FromId = ConfigurationManager.AppSettings["Twilio_PhoneNumber"],
                 ToId = phoneNumber,
-                Text = string.Format("Hello, {0}!. This is Rachael from Express. It is my pleasure to inform you that you have been accepted for the position '{1}' at {2}. I'd like to walk you through filling out your IRS W-9 form. We are required to get this information from you before you can start your position. To get started, please add me to your contacts in skype.", 
+                Text = string.Format("Hello, {0}!. This is Rachael from Express. It is my pleasure to inform you that you have been accepted for the position '{1}' at {2}. I'd like to walk you through filling out your IRS W-4 form. We are required to get this information from you before you can start your position. To get started, please add me to your contacts in skype.", 
                     a.Name, app.Title, app.Company),
                 ServiceUrl = ConfigurationManager.AppSettings["BotFramework_SmsServiceUrl"]
             };
