f42ec0b0598547f60d019d1f2eb6806cb7a1985b
nhaq <nhaq@infusion.com>
2017-04-10 23:28:22 -0500

    set the conversation to none

--- X-Bot First Class/Dialogs/ScheduleInterviewForm.cs
+++ X-Bot First Class/Dialogs/ScheduleInterviewForm.cs
@@ -100,6 +100,7 @@ namespace X_Bot_First_Class.Dialogs
             context.UserData.TryGetValue<Applicant>("applicant", out a);
             var applicationKey = a.Applications.Keys.First();
             a.Applications[applicationKey].Interview = state.ChoosenDate;
+            a.Applications[applicationKey].State = ConversationType.None;            
             await ApplicantFactory.PersistApplicant(a);
             await context.PostAsync($"Please click here to add the invitation to your calendar: http://x-bot-first-class.azurewebsites.net/api/ics?&apptDateTime={state.ChoosenDate:s}");
             
